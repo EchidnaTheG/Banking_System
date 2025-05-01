b3f7c5259816ba34d2bd6972ad85ffd3ced28e63
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-05-01 03:59:35 -0300

    added account skeleton class

--- app/models/account.py
+++ app/models/account.py
@@ -1,2 +1,9 @@
 class Account:
-    
+    def __init__(self,account_num, account_type, balance, currency, status, opening_date, owner):
+        self.account_num = account_num
+        self.type = account_type
+        self.balance = balance 
+        self.currency = currency
+        self.status= status
+        self.opening_date= opening_date
+        self.owner = owner
